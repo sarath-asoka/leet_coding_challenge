24c8c53c85d3e99742d10652576747c41d48e034
sarathasoka <55868074+sarath-asoka@users.noreply.github.com>
2023-06-14 15:38:05 +0900

    Update find_duplicates.py

--- find_duplicates.py
+++ find_duplicates.py
@@ -5,8 +5,8 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        print(len(set(nums)))
-        print(len(nums))
+        # print(len(set(nums)))
+        # print(len(nums))
         return len(set(nums)) < len(nums)                   
         
 x = [1,2,3,1,1,1]       
